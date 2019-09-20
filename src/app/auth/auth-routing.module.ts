0dc748b6fbb3e2b490d1dd865bfcb216ced6e509
Valeriy Garbuzov <valeriy@hydraglobe.com>
2019-09-20 23:55:31 +0300

    applying auth design, updated auth-routs

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 
 const routes: Routes = [
-  {path: 'login',         component: LoginComponent},
-  {path: 'registration',  component: RegistrationComponent}
+  {path: '', component: AuthComponent, children: [
+      {path: 'login',         component: LoginComponent},
+      {path: 'registration',  component: RegistrationComponent}
+    ]},
 ];
 
 @NgModule({
