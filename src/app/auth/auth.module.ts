1e5c9f2e25c62859054d677fb581b3f9a5a33f15
Valeriy Garbuzov <valeriy@hydraglobe.com>
2019-09-21 13:39:45 +0300

    added form validators and hints

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
@@ -12,7 +14,9 @@ import { RegistrationComponent } from './registration/registration.component';
     AuthComponent
   ],
   imports: [
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
 
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form = this.fb.group({
+    email: this.fb.control(null, [Validators.required, Validators.email]),
+    password: this.fb.control(null, [Validators.required, Validators.minLength(6)])
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  hasFormError() {
+    return this.form.invalid && this.form.touched;
+  }
+
+  showErrorMessage(error: string, e?: {requiredLength?: number}) {
+    switch (error) {
+      case 'required': return 'Обовʼязвове поле.';
+      case 'email': return 'Не коректний Email';
+      case 'minlength': return `Довжина поля має бути не меншою за ${e.requiredLength} символів`;
+      default: return '';
+    }
+  }
+
+  onSubmit() {
+    console.log(this.form);
   }
 
 }
