cf062eb6cd5b2dae268946d9d8f1ede3519c0e30
Valerii Harbuzov <valeriy.garbuzov@gmail.com>
2026-07-19 20:42:06 +0300

    chore(web): remove dead Nx-scaffold leftovers from i18n migration

--- libs/identity/feature-access-status/src/lib/identity-feature-access-status/identity-feature-access-status.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-identity-feature-access-status',
-  imports: [],
-  templateUrl: './identity-feature-access-status.html',
-  styleUrl: './identity-feature-access-status.scss',
-})
-export class IdentityFeatureAccessStatus {}
--- libs/identity/feature-greeting/src/lib/identity-feature-greeting/identity-feature-greeting.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-identity-feature-greeting',
-  imports: [],
-  templateUrl: './identity-feature-greeting.html',
-  styleUrl: './identity-feature-greeting.scss',
-})
-export class IdentityFeatureGreeting {}
